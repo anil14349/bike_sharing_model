374e9e366f3ba0f714d6fef77f7d35e17aab80dd
Anil Kumar <anil14349@gmail.com>
2024-06-25 09:17:51 +0530

    updated pipeline code

--- bike_sharing_model/pipeline.py
+++ bike_sharing_model/pipeline.py
@@ -9,16 +9,17 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 
 from bike_sharing_model.config.core import config
-from bike_sharing_model.processing.features import WeekdayImputer, OutlierHandler, WeathersitImputer, WeekdayOneHotEncoder
+from bike_sharing_model.processing.features import WeekdayImputer
+from bike_sharing_model.processing.features import OutlierHandler
+from bike_sharing_model.processing.features import WeathersitImputer
+from bike_sharing_model.processing.features import WeekdayOneHotEncoder
 from bike_sharing_model.processing.features import Mapper
-from sklearn.impute import SimpleImputer
+
 
 
 bike_sharing_pipe=Pipeline([
     ("WeathersitImputer", WeathersitImputer(variables=config.model_config.weathersit_var)),
     ("WeekdayImputer", WeekdayImputer(variables=config.model_config.weekday_var)),
-    ('outlers', OutlierHandler()),
-    #('holiday_imputer', SimpleImputer(strategy='most_frequent')),
      ##==========Mapper======##
      ("map_yr", Mapper(config.model_config.yr_var, config.model_config.yr_mapping)),
      ("map_mnth", Mapper(config.model_config.mnth_var, config.model_config.mnth_mapping)),
@@ -27,9 +28,10 @@ bike_sharing_pipe=Pipeline([
      ("map_workingday", Mapper(config.model_config.workingday_var, config.model_config.workingday_mapping)),
      ("map_hr", Mapper(config.model_config.hr_var, config.model_config.hr_mapping)),
      ("map_season", Mapper(config.model_config.season_var, config.model_config.season_mapping)),
-     ("weekday_mappings", Mapper(config.model_config.weekday_var, config.model_config.weekday_mapping)),
+     # ("weekday_mappings", Mapper(config.model_config.weekday_var, config.model_config.weekday_mapping)),
+     ('outlers', OutlierHandler()),
      # Transformation of age column
-     #("age_transform", age_col_tfr(config.model_config.age_var)),
+     ('weekday_encode', WeekdayOneHotEncoder(column_name='weekday')),
      # scale
      ("scaler", StandardScaler()),
      ('model_rf', RandomForestRegressor(n_estimators=config.model_config.n_estimators, max_depth=config.model_config.max_depth,random_state=config.model_config.random_state))
